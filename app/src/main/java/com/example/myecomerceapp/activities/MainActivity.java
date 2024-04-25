d6877548640e73b30d605ae452bc2845fa5ca28c
Amarusinggithub <loveamar285@gmail.com>
2024-04-24 19:39:32 -0500

    removed unused imports

--- app/src/main/java/com/example/myecomerceapp/activities/MainActivity.java
+++ app/src/main/java/com/example/myecomerceapp/activities/MainActivity.java
@@ -13,7 +13,6 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
